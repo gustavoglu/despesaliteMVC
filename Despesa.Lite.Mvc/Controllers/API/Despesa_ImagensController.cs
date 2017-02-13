6c2559b8fb24446650b7158e23402e39dc10d5ee
Gustavo Giroldo <gustavoglu@hotmail.com>
2017-02-13 11:26:45 -0200

    Adicionado Metodo de Upload na Api de Despesa_Imagens

--- Despesa.Lite.Mvc/Controllers/API/Despesa_ImagensController.cs
+++ Despesa.Lite.Mvc/Controllers/API/Despesa_ImagensController.cs
@@ -12,6 +12,8 @@ using Despesa.Lite.Mvc.Application.ViewModels;
 using Despesa.Lite.Mvc.Models;
 using Despesa.Lite.Mvc.Application.Interfaces;
 using Despesa.Lite.Mvc.Application.Services;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace Despesa.Lite.Mvc.Controllers.API
 {
@@ -92,7 +94,102 @@ namespace Despesa.Lite.Mvc.Controllers.API
             return Ok(despesa_ImagemViewModel);
         }
 
-        protected override void Dispose(bool disposing)
+        [AllowAnonymous]
+        public Dictionary<bool,string> PostUserImage()
+        {
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+
+            Dictionary<bool, string> retorno = new Dictionary<bool, string>();
+
+            try
+            {
+                var username = HttpContext.Current.User.Identity.Name;
+
+                var user = db.Users.SingleOrDefault(u => u.UserName == username);
+
+                string filepath = "";
+
+                string filename = string.Format("{0}_{1}",user.UserName, DateTime.Now.Date.ToString());
+
+                var httpRequest = HttpContext.Current.Request;
+
+                foreach (string file in httpRequest.Files)
+                {
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created);
+
+                    var postedFile = httpRequest.Files[file];
+                    if (postedFile != null && postedFile.ContentLength > 0)
+                    {
+
+                        int MaxContentLength = 1024 * 1024 * 5; //Size = 5 MB  
+
+                        IList<string> AllowedFileExtensions = new List<string> { ".jpg", ".png" };
+
+                        var ext = postedFile.FileName.Substring(postedFile.FileName.LastIndexOf('.'));
+
+                        var extension = ext.ToLower();
+
+                        if (!AllowedFileExtensions.Contains(extension))
+                        {
+
+                            var message = string.Format("Please Upload image of type .jpg,.gif,.png.");
+
+                            dict.Add("error", message);
+
+                            retorno.Add(false, message);
+
+                            return retorno;
+                        }
+                        else if (postedFile.ContentLength > MaxContentLength)
+                        {
+
+                            var message = string.Format("Upload de imagens até 5MB");
+
+                            dict.Add("error", message);
+
+                            retorno.Add(false, message);
+
+                            return retorno;
+
+                        }
+                        else
+                        {
+
+                            filepath = HttpContext.Current.Server.MapPath("~/DespesaImagens/" + filename + extension);
+
+                            postedFile.SaveAs(filepath);
+
+                        }
+                    }
+
+                    var message1 = string.Format("Realizado Upload");
+
+                    retorno.Add(true, filepath);
+
+                    return retorno;
+                }
+                var res = string.Format("Faça upload de uma Imagem");
+                dict.Add("error", res);
+
+                retorno.Add(false, res);
+
+                return retorno;
+            }
+            catch (Exception ex)
+            {
+                var res = string.Format("some Message");
+                dict.Add("error", res);
+
+                retorno.Add(false, ex.Message);
+
+                return retorno;
+            }
+        }
+    
+
+
+
+    protected override void Dispose(bool disposing)
         {
             if (disposing)
             {
@@ -100,6 +197,8 @@ namespace Despesa.Lite.Mvc.Controllers.API
             }
             base.Dispose(disposing);
         }
+
+
 
         private bool Despesa_ImagemViewModelExists(Guid id)
         {
